721b654b0de730ecd80a0e74eac918dc886167a4
Tetiana Karlova <tanyalyt80@gmail.com>
2023-09-20 21:23:37 +0300

    add project

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { PersistGate } from 'redux-persist/integration/react';
+import { App } from 'components/App';
+import './index.css';
+import { Provider } from 'react-redux';
+import { persistor, store } from 'redux/store';
+import { BrowserRouter } from 'react-router-dom';
+import Loader from 'components/Loader/Loader';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={<Loader />} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
+);
